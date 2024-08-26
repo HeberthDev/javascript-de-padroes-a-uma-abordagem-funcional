003d437ba9b4c30d27d6d92f6d4792919e5b4d64
HeberthDev <heberthdeveloper@gmail.com>
2024-08-26 07:46:42 -0300

    Consumindo endpoint REST com a API Fetch

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,1 +1,7 @@
-document.querySelector('#myButton').onclick = () => alert('Oi');
+import { handleStatus } from "./utils/promise-helpers.js";
+
+document.querySelector('#myButton').onclick = () =>
+    fetch('http://localhost:3000/notas')
+    .then(handleStatus)
+    .then(notas => console.log(notas))
+    .catch(error => console.log(error));
